e5619980f759d515624d09c91d931e998e259fd8
Benjamin Schleiss <benjamin_schleiss@sluz.ch>
2022-11-21 14:50:56 +0100

    Added Main Menu

---
+++ Assets/Scripts/Menu/StartGame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+  
+        public void GameSceneLoad()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
