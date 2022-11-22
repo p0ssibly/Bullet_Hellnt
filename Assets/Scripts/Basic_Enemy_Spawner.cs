893c4fb2f96db34e25890434efa381d7f199b8dc
Benjamin Schleiss <benjamin_schleiss@sluz.ch>
2022-11-22 08:49:41 +0100

    Refactored enemy spawner

--- Assets/Scripts/Basic_Enemy_Spawner.cs
+++ Assets/Scripts/Basic_Enemy_Spawner.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Basic_Enemy_Spawner : MonoBehaviour
 {
-  void Start()
-  {
-  }
-
-  void Update()
-  {
-    timer += Time.deltaTime;
-    if (timer >= interval)
+    void Start()
     {
-      Vector2 randomPosition = origin + Random.insideUnitCircle * radius;
-      Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
-      timer -= interval;
     }
-  }
 
-  public Vector2 origin = Vector2.zero;
-  public float radius = 10;
-  public GameObject objectToSpawn;
-  public float interval = 5;
-  float timer;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+
+            Vector2 randomPosition = (Vector2)origin.transform.position + Random.insideUnitCircle * radius + offset;
+            Instantiate(objectToSpawn, randomPosition, Quaternion.identity,transform);
+            timer -= interval;
+        }
+    }
+
+    public Vector2 offset = Vector2.zero;
+    public Transform origin;
+    public float radius = 10;
+    public GameObject objectToSpawn;
+    public float interval = 5;
+    float timer;
+
 }
