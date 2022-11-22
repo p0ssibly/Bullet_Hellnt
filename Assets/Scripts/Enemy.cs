b729e9d6138dfefc61ab0aa2f9eba2dc088fcec7
Luca Rickenbach <Luca_Rickenbach@sluz.ch>
2022-11-22 08:15:27 +0100

    Enemy animation & healthbar done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -46,7 +46,7 @@ public class Enemy : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("PLAYER HIT");
-        takeDamage(30);
+        takeDamage(40);
     }
 
     void takeDamage(int damage)
